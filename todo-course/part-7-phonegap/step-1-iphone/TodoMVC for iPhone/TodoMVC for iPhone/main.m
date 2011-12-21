f6191b92c427869218179a4ebc5267d2fc11ffff
Cat Chen <cathsfz@gmail.com>
2011-12-21 15:05:53 +0800

    added initial phonegap project

---
+++ todo-course/part-7-phonegap/step-1-iphone/TodoMVC for iPhone/TodoMVC for iPhone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TodoMVC for iPhone
+//
+//  Created by Chen Guangchen on 11-12-21.
+//  Copyright Wandou Labs 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
